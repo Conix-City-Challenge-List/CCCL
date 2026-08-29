5686ecf02b6c925699da42e95cd58401c68d307f
Duckly <realactethx@gmail.com>
2026-08-30 02:14:28 +0300

    Update PackDifficulty.js

--- js/components/Sidebar/PackDifficulty.js
+++ js/components/Sidebar/PackDifficulty.js
@@ -3,19 +3,34 @@ export default {
         <h3>Difficulty Rankings</h3>
         <div class="right-text">
             <p>
-                Legendary Packs = Packs with levels from the supreme tier and above (500 points)
+                Silent Packs = Packs with challenges from the Silent tier (500 points)
             </p>
             <p>
-                Extreme Packs = Packs with levels from the extreme tier (250 points)
+                Legendary Packs = Packs with challenges from the Legendary tier (400 points)
             </p>
             <p>
-                Mythical Packs = Packs with levels from the mythical tier (150 points)
+                Etheral Packs = Packs with challenges from the Etheral tier (300 points)
             </p>
             <p>
-                Insane Packs = Packs with levels from the insane tier (100 points)
+                Supreme Packs = Packs with challenges from the Supreme tier (250 points)
             </p>
             <p>
-                Hard Packs = Packs with levels from the hard tier (75 points)
+                Extreme Packs = Packs with challenges from the Extreme tier (200 points)
+            </p>
+            <p>
+                Mythical Packs = Packs with challenges from the Mythical tier (150 points)
+            </p>
+            <p>
+                Insane Packs = Packs with challenges from the Insane tier (100 points)
+            </p>
+            <p>
+                Hard Packs = Packs with challenges from the Hard tier (75 points)
+            </p>
+             <p>
+                Medium Packs = Packs with challenges from the Medium tier (50 points)
+            </p>
+               <p>
+                Easy Packs = Packs with challenges from the Easy tier (25 points)
             </p>
         </div>
     `
