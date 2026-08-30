8f79c390db5c8bc505ef5ff6ca6bfc7c0f5de8dc
Duckly <realactethx@gmail.com>
2026-08-30 13:43:09 +0300

    Update Level.js

--- js/components/List/Level.js
+++ js/components/List/Level.js
@@ -1,4 +1,5 @@
 import { copyURL } from "../../util.js";
+import { legacyLimit } from "../../config.js";
 import LevelAuthors from "./LevelAuthors.js";
 import Copy from "../Copy.js";
 import Copied from "../Copied.js";
@@ -38,13 +39,19 @@ export default {
             </div>
             <Packs :packs="level.packs" v-show="!fromPacksPage" v-if="level.packs.length > 1 || level.packs.length !== 0 && level.packs[0].levels" />
             <LevelAuthors :creators="level.creators" :verifier="level.verifier" :enjoyment="level.enjoyment"></LevelAuthors>
-            <h3>Difficulty: {{["Beginner", "Easy", "Medium", "Hard", "Insane", "Mythical", "Extreme", "Supreme", "Ethereal", "Legendary", "Silent", "Impossible"][level.difficulty]}} Challenge</h3>
+            <h3 v-if="isLegacy">Difficulty: Legacy</h3>
+            <h3 v-else>Difficulty: {{["Beginner", "Easy", "Medium", "Hard", "Insane", "Mythical", "Extreme", "Supreme", "Ethereal", "Legendary", "Silent", "Impossible"][level.difficulty]}} Challenge</h3>
             <Verification :verification="level.verification" :showcase="level.showcase || null" />
             <LevelMeta :level="level" :list="list" />
-            <Records :records="level.records" :percentToQualify="(level.difficulty>3) ? level.percentToQualify : 100" />
+            <Records :records="level.records" :percentToQualify="(level.difficulty>3) ? level.percentToQualify : 100" :isLegacy="isLegacy" />
         </div>
     `,
     methods: {
         copyURL,
+    },
+    computed: {
+        isLegacy() {
+            return this.level.rank !== null && this.level.rank > legacyLimit;
+        },
     },
 }
